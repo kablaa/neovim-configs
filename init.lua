93c27f7fe69f8a5686e2947efbbcc798e567ec41
kablaa <tlukasiewicz7993@gmail.com>
2025-05-18 14:12:10 -0400

    adding back the split-scren mappings

--- init.lua
+++ init.lua
@@ -165,8 +165,8 @@ vim.opt.tabstop = 2
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 
-vim.keymap.set('n', '<C-l>', 'gt')
-vim.keymap.set('n', '<C-h>', 'gT')
+-- vim.keymap.set('n', '<C-l>', 'gt')
+-- vim.keymap.set('n', '<C-h>', 'gT')
 
 -- Popup lazygit terminal
 vim.keymap.set('n', '<C-g>', function()
@@ -208,10 +208,10 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
--- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
--- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
--- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
--- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
